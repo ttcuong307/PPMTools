ad13b08f4d7962d867df586aa3a0ddd3f3e4b929
ttcuong307 <ttcuong307@gmail.com>
2021-03-25 22:44:14 +0700

    first commit

---
+++ src/test/java/io/cuongtran/ppmt/PpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cuongtran.ppmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
